5913f13a266b6e6e840613a223315b1c0969a256
Akira <wulcat@icloud.com>
2022-07-25 20:41:10 +0530

    Well as expected setting up a car is bit time consuming, will get back to this on once i have enough time to rnd :/

--- Source/Vehicles/Private/Car.cpp
+++ Source/Vehicles/Private/Car.cpp
@@ -9,6 +9,52 @@ ACar::ACar()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	if(!RootComponent)
+	{
+		RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	}
+
+	if(!CollisionComponent)
+	{
+		CollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionComponent"));
+
+		RootComponent = CollisionComponent;
+	}
+
+	if(!MeshComponent)
+	{
+		MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	}
+	
+	if(!CenterOfMass)
+	{
+		CenterOfMass = CreateDefaultSubobject<UArrowComponent>(TEXT("Center of Mass"));
+	}
+
+	if(WheelComponents.IsEmpty())
+	{
+		auto WheelActor = CreateDefaultSubobject<UChildActorComponent>(TEXT("Wheel_RL"));
+		WheelComponents.Add(WheelActor);
+
+		WheelActor = CreateDefaultSubobject<UChildActorComponent>(TEXT("Wheel_RR"));
+		WheelComponents.Add(WheelActor);
+
+		WheelActor = CreateDefaultSubobject<UChildActorComponent>(TEXT("Wheel_TL"));
+		WheelComponents.Add(WheelActor);
+
+		WheelActor = CreateDefaultSubobject<UChildActorComponent>(TEXT("Wheel_TR"));
+		WheelActor->SetRelativeLocation(FVector(100,-100,0));
+		WheelComponents.Add(WheelActor);
+	}
+	else
+	{
+		for(const auto WheelActor : WheelComponents)
+		{
+			WheelActor->SetChildActorClass(AWheelMesh);
+		}
+	}
+
+	
 }
 
 // Called when the game starts or when spawned
--- Source/Vehicles/Public/Car.h
+++ Source/Vehicles/Public/Car.h
@@ -2,7 +2,11 @@
 
 #pragma once
 
+#include <array>
+
 #include "CoreMinimal.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Car.generated.h"
 
@@ -16,14 +20,76 @@ public:
 	ACar();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+public:
+	UPROPERTY(EditAnywhere)
+	bool bIsTurningAllowed = false;
 
+	UPROPERTY(EditAnywhere)
+	float EngineForce = 2500; // engine torque = 440 rpm
+	UPROPERTY(EditAnywhere)
+	float BrakeForce = 1000; // uses same torque as engine force 
+
+	UPROPERTY(EditAnywhere)
+	float Mass = 1500; // kg
+
+	UPROPERTY(EditAnywhere)
+	float CAirDrag = 0.4257f; 
+	UPROPERTY(EditAnywhere)
+	float CRollingResistance = 12.8f;
+	UPROPERTY(EditAnywhere)
+	float CCenterOfMassHeight = 5 ; // arbitrary constant added to total height of center of mass from ground while resting on ground
+
+	UPROPERTY(EditAnywhere)
+	TArray<float>Gears{2.66f, 1.78f, 1.3f, 1, 0.74f, 0.5f};
+
+	UPROPERTY(EditAnywhere)
+	float ReverseGear = 2.9f;
+	UPROPERTY(EditAnywhere)
+	float DifferentialRatio = 3.42f;
+	UPROPERTY(EditAnywhere)
+	float TransmissionRatio = 0.7f; // torque from engine is converted via gear and differential ratio where energy is lost in form of heat (around 30%) so we get 70% efficacy
+
+	
+	UPROPERTY(EditAnywhere)
+	float CWheelFriction = 1;
+	UPROPERTY(EditAnywhere)
+	float SteerRotationLimit = 25; // in degrees
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> AWheelMesh;
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<AActor>> Wheels;
+	
+protected:
+	UPROPERTY(VisibleAnywhere)
+	FVector Acceleration; // m/s*s
+	UPROPERTY(VisibleAnywhere)
+	FVector Velocity; // m/s
+	UPROPERTY(VisibleAnywhere)
+	FVector DriveForce; // calculated when vehicle is resting on ground
+
+	UPROPERTY(VisibleAnywhere)
+	float Speed;
+	UPROPERTY(VisibleAnywhere)
+	float MaxCenterOfMassHeight;
+	UPROPERTY(VisibleAnywhere)
+	float MaxTractionForce;
+	UPROPERTY(VisibleAnywhere)
+	float WheelBase;
+	
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	UArrowComponent* CenterOfMass;
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<UChildActorComponent*> WheelComponents;
 };
